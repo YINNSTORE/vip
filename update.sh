312d14eebbcdc9ec1e1422a2766eec40aa4553a4
YINNSTORE <146443900+YINNSTORE@users.noreply.github.com>
2023-12-09 11:26:18 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/victor3232/vip/main/limit/menu.zip
+    wget https://raw.githubusercontent.com/YINNSTORE/vip/main/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
